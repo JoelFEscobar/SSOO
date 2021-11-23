03d5c1665a09089629edf741e803675bf0af106a
Joel <alu0101130408@ull.edu.es>
2021-11-23 17:38:36 +0000

    Caso base mejorado con estructura del programa

--- userProc.sh
+++ userProc.sh
@@ -54,7 +54,104 @@ lista(){
 
 }
 
-lista
+#Función error_exit que controla la salida de errores
+error_exit(){
+  case $1 in
+    1) echo "$TEXT_MAGENTA Error!: Opcion seleccionada no valida$TEXT_RESET" 1>&2
+			help_func
+			exit 1
+			;;
+
+    2) echo "$TEXT_MAGENTA Error!: Los argumentos introducidos son incompatibles$TEXT_RESET" 1>&2
+      help_func
+      exit 1
+      ;;
+
+    3) echo "$TEXT_MAGENTA Error!: Formato del número N no válido $TEXT_RESET" 1>&2
+      format_case
+      help_func
+      exit 1
+      ;;
+    4) echo "$TEXT_MAGENTA Error!: opcion no encontrada $TEXT_RESET" 1>&2
+      help_func
+      exit 1
+      ;;
+    *) echo "$TEXT_MAGENTA Error Desconocido $TEXT_RESET" 1>&2
+      help_func
+      exit 1
+      ;;
+  esac
+
+}
+
+#Función que muestra el formato deseado 
+format_case(){
+  echo "El formato del número introducido debe ser: $TEXT_CIAN 00:00:00 $TEXT_RESET "
+  echo "══════════════════════════════════════════════════════════════════════════════"
+  echo "Donde: "
+  echo "→ Los primeros $TEXT_CIAN 00:$TEXT_RESET corresponden a horas"
+  echo "→ Los segundos $TEXT_CIAN :00:$TEXT_RESET corresponden a minutos"
+  echo "→ Los terceros $TEXT_CIAN :00$TEXT_RESET corresponden a segundos"
+  echo "→ Por defecto el valor será 00:00:01, correspondiente a 1 segundo"
+  echo "══════════════════════════════════════════════════════════════════════════════"
+}
+
+#Función Help que muestra el uso del script
+
+help_func(){
+
+  echo "Modo de Uso: ./userProc.sh [Options] "
+  echo "══════════════════════════════════════════════════════════════════════════════"
+  echo "Las opciones soportadas son: "
+  echo "→ Sin argumentos: Mostrará los usuarios que están ejecutando proceso con un tiempo de cpu consumido mayor que 00:00:01"
+  echo "→ -h o --help: Mostrará una ayuda acerca del funcionamiento"
+  echo "══════════════════════════════════════════════════════════════════════════════"
+
+}
+
+
+
+# #────────────────────────┤ Llamada al Main ├──────────────────────────────
+
+clear
+while [ "$1" != "" ]; do
+  case $1 in
+    --help | -h )
+      echo " ─┤ Ayuda del Programa ├─"
+      opcion_help=1	
+      ;;
+
+    -t )
+      echo " Seleccionando el Número"
+      opcion_t=1
+      shift
+      N=$1
+      ;;
+
+    * )
+      error_exit 1
+      exit 4
+      ;;
+
+    esac
+    shift
+done
+
+#────────────────────────┤ Gestión de las opciones pasadas por línea de comando ├──────────────────────────────
+#Opcion por defecto (00)
+if [ "$opcion_help" = "0" ] && [ "$opcion_t" = "0" ]; then
+lista|uniq
+# Si solo llamamos a ayuda (10)
+elif [ "$opcion_help" = "1" ] && [ "$opcion_t" = "0" ]; then
+help_func
+# Si le pasamos el número de segundos a filtrar (01)
+elif [ "$opcion_help" = "0" ] && [ "$opcion_t" = "1" ]; then
+lista |uniq
+echo "N sera: $N"
+
+else
+error_exit 2
+fi
 
 #────────────────────────┤ Fin del programa ├──────────────────────────────
 
